25c474bc60577d50d05eff8875300ab7b7c60d5d
unknown <oussamajbeliiset@gmail.com>
2022-10-12 22:15:29 +0100

    first commit

---
+++ produits/src/main/java/com/oussama/produits/service/ProduitService.java
@@ -0,0 +1,11 @@
+package com.oussama.produits.service;
+import java.util.List;
+import com.oussama.produits.entities.Produit;
+public interface ProduitService {
+Produit saveProduit(Produit p);
+Produit updateProduit(Produit p);
+void deleteProduit(Produit p);
+void deleteProduitById(Long id);
+Produit getProduit(Long id);
+List<Produit> getAllProduits();
+}
